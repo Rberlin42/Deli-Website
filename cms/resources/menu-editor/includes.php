b14b46be244c824ca28a9757439133351625dba7
Ryan Berlin <ryanaberlin@gmail.com>
2020-10-22 14:42:12 -0400

    delete old files

--- cms/resources/menu-editor/includes.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" type="text/css" href="/cms/resources/style.css">
-<link rel="stylesheet" type="text/css" href="/cms/resources/menu-editor/menu-editor-style.css">
-<link href="https://fonts.googleapis.com/css?family=Noto+Sans&display=swap" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-<script src="https://kit.fontawesome.com/b795fdd398.js" crossorigin="anonymous"></script>
-<script src="/node_modules/sortablejs/Sortable.js"></script>
-<script src="/cms/resources/menu-editor/menu-editor-script.js"></script>
